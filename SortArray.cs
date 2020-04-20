f0922419eac8a5708eb7fd398b2441d5cf3145c0
Truong Duong <tanduongbk@gmail.com>
2020-04-20 22:42:49 +0700

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -266,4 +266,25 @@ public class SortArray
 			}			
 		}		
 	}
+	
+	/* BubbleSort(int[] A)
+	 * Description: Sort an array ascending order using bubble sort algorithm
+	 * Param (int[] A): non-sorted array		
+	 * Return: Non
+	 */
+	public void BubbleSort(int[] A)
+	{
+		for (int i = 0; i < A.Length-1; i++)
+		{
+			for (int j = A.Length - 1; j >= i + 1; j--)
+			{
+				if (A[j] < A[j-1])
+				{
+					int temp = A[j];
+					A[j] = A[j-1];
+					A[j-1] = temp;
+				}
+			}			
+		}
+	}
 }
